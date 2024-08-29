e8b857a842bdd6be0bdcee4af26fb10afa45ee4d
fauzanazmi01 <fauzan.azmi01@gmail.com>
2024-08-29 15:04:13 +0700

    create the required migration and model

---
+++ database/migrations/2024_08_28_091006_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Container\Attributes\DB;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedInteger('quantity');
+            $table->decimal('total_price', 10, 2);
+            $table->string('customer_name');
+            $table->text('customer_address');
+            $table->timestamp('order_date')->useCurrent();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+
+            $table->foreign('product_id')
+                  ->references('id')
+                  ->on('products')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
