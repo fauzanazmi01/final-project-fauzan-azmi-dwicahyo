e6bbc03c5f439c77f0c90e7ee96c2dc5deb4f1aa
fauzanazmi01 <fauzan.azmi01@gmail.com>
2024-08-29 15:56:35 +0700

    use the short name

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('categories', \App\Http\Controllers\CategoryController::class);
+Route::resource('categories', CategoryController::class);
 Route::resource('products', ProductController::class);
 
 Route::group([
