1280a57ca00d8a284931a967f6d6c4bd2406ea2e
fauzanazmi01 <fauzan.azmi01@gmail.com>
2024-08-29 16:35:19 +0700

    move address field to user table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'address' => fake()->address(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
---
+++ database/migrations/2024_08_29_092436_move_address_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('customer_name');
+            $table->dropColumn('customer_address');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('address');
+        });
+
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('customer_name');
+            $table->text('customer_address');
+        });
+    }
+};
