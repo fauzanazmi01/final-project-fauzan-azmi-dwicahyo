20e4fef0cd8a243d0294018da52f2024c957a042
fauzanazmi01 <fauzan.azmi01@gmail.com>
2024-08-30 08:17:26 +0700

    fix missing route

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::middleware([
 Route::group([
     'middleware' => 'api',
 ], function () {
+    Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
